8c57a58de59ccaab72209812707cb6ea4545d0d7
sneha gupta <snehagupta4513@gmail.com>
2024-05-02 21:04:48 +0530

    changes

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "./components/Header";
+import { ReactElement } from "react";
+
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
